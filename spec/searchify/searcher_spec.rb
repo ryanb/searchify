16c40f2dd430d3589ec38442f1f35a0f31a79ee6
Ryan Bates <rbates@artbeats.com>
2008-03-17 11:21:58 -0700

    ensuring the searcher doesn't alter the options hash passed in

--- spec/searchify/searcher_spec.rb
+++ spec/searchify/searcher_spec.rb
@@ -5,6 +5,14 @@ describe Searchify::Searcher do
     MockedModel.reset_columns
   end
   
+  it "should not alter the options hash that is being passed in" do
+    MockedModel.add_column(:name)
+    searcher = Searchify::Searcher.new(MockedModel, :name)
+    options = { :foo => 'bar' }
+    searcher.search(options)
+    options.should == { :foo => 'bar' }
+  end
+  
   it "should included specified association in search" do
     MockedModel.add_column(:name)
     MockedModel.has_many(:mocked_models)
